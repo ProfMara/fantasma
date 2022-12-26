d00878fca85f6fe324f5b3984d0e5be17d3d4712
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-12-26 14:51:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,19 +8,21 @@ var somAssustador;
 var playerparado;
 
 function preload() {
-    //carregar as imagens da torre, da grade e da porta
+    //é assim que carrega a imagem da torre
     torreImg = loadImage("torre.png");
+    //agora, do mesmo jeito, carregue as imagens da grade e da porta
 
-
+    
+    
     //carregando as animações do fantasma
     player_parado = loadAnimation("fantasma parado.png");
     player_pulando = loadAnimation("fantasma pulando.png");
 
-    //carregar som
+    //programe para carregar o som
 
-    
+    //é assim que cria um grupo
     grupoBlocoInvisivel = new Group()
-    //criar os grupos das grades e das portas
+    //agora, crie aqui os grupos das grades e das portas
     
 }
 
@@ -53,21 +55,22 @@ function draw() {
 
         
 
-        //código para controlar o jogador para Esquerda
+        //esse é o código para controlar o jogador para Esquerda
         if (keyDown("left")) {
             player.x -= 3;
             player.changeAnimation("player pulando");
         }
 
-        //código para controlar o jogador para direita
+        //agora, adicione o código para controlar o jogador para direita
 
         
-        //gravidade
+        //do mesmo jeito que adicionou a gravidade do trex, adicione a do player.
 
-        //Código para reiniciar a torre
+        
+        //Do mesmo jeito que fez um loop infinito para o solo do trex, adicione o código para a torre estar em loop infinito
 
 
-        //é aqui que programa para o player colidir com as grades
+        //programe para o player colidir com as grades
 
         //é aqui que chama a função gerarPortas()
  
@@ -97,27 +100,28 @@ function gerarPortas() {
 
         grade = createSprite(porta.x, 10);
 
-        blocoInvisivel = createSprite(porta.x, 15, grade.width, 2);
+        blocoInvisivel = createSprite(porta.x + 10, 15, grade.width, 2);
 
-        //fazer o bloco invisivel ser invisivel
+        //adicione o código para o bloco invisivel ser invisivel
 
 
-        //código para adicionar as imagens nas sprites
+        //adicione o código para adicionar as imagens nas sprites
 
         
            
-        //velocidade das sprites
+        //é assim que se dá velocidade para o bloco
         blocoInvisivel.velocityY = 1;
+        //agora, dê velocidade para a porta e para a grade
 
-
-        //tempo de vida das sprites
+        //é assim que se dá tempo de vida para a sprite
         blocoInvisivel.lifetime = 800;
+        //agora, dê tempo de vida para a porta e a grade
         
-        //programe para que o player esteja na frente  do jogador
+        //programe para que o player esteja na frente  do jogador aumentando seu depth
 
-        //adicionar nos grupos as sprites
+        //é assim que se adiciona uma sprite no grupo
         grupoBlocoInvisivel.add(blocoInvisivel);
-        
+        //agora, adicione a sprite de porta no grupo das portas e a sprite de grade no grupo das grades.
 
 
     }
